5a39ffe566474733e3eaa5044de4f9e014dd6671
ElisaMamolo <elisa.carlotta.mamolo@lego.com>
2022-01-13 11:34:58 +0100

    obstacles, starting crash handling

--- codealong/js/script.js
+++ codealong/js/script.js
@@ -1,17 +1,24 @@
+const myObstacles = []; //store all created obstacles
+
 const myGameArea = {
   canvas: document.createElement("canvas"),
+  //track how many times the canvas is updated
+  frames: 0,
   start: function () {
     this.canvas.width = 480;
     this.canvas.height = 270;
     this.context = this.canvas.getContext("2d");
     //insert canvas inside of the bdy as its first child
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    //call updategamearea every 20 seconds
+    //call update game area every 20 seconds
     this.interval = setInterval(updateGameArea, 20);
   },
   clear: function () {
     //get context and clear canvas
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+  stop: function () {
+    clearInterval(this.interval);
   },
 };
 
@@ -35,10 +42,29 @@ class Component {
   }
 
   update() {
-    //take x,y, width and heigh and create watever is in x and y
+    //take x,y, width and heigh and create whatever is in x and y
     const ctx = myGameArea.context;
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  left() {
+    return this.x;
+  }
+  right() {
+    return this.x + this.width;
+  }
+  top() {
+    return this.y;
+  }
+  bottom() {
+    return this.y + this.height;
+  }
+ 
+  crashWith(obstacle) {
+      //method for class component, we pass the object obstacle
+      //returns a boolean if we crashed or not
+    return !(this.bottom() < obstacle.top() || this.top() > obstacle.bottom() || this.right() < obstacle.left() || this.left() > obstacle.right());
   }
 }
 
@@ -76,6 +102,39 @@ function updateGameArea() {
   player.newPos();
   //update component
   player.update();
+  //update obstacle
+  this.updateObstacles();
+}
+
+function updateObstacles() {
+  for (i = 0; i < myObstacles.length; i++) {
+    myObstacles[i].x += -1;
+    myObstacles[i].update();
+  }
+  myGameArea.frames += 1;
+  //counter for the frames, incremented by 1 each time
+  if (myGameArea.frames % 120 === 0) {
+    //This condition will determine every how many update we create new obstacles.
+    //We set every 120 updates, that means 2.4 seconds,
+    //because we call the updateGameArea() function every 20 milliseconds.
+    let x = myGameArea.canvas.width;
+    let minHeight = 20;
+    let maxHeight = 200;
+    let height = Math.floor(
+      Math.random() * (maxHeight - minHeight + 1) + minHeight
+    );
+    //randomized height between 20 and 200 to be the height for the obstacles
+    let minGap = 50;
+    let maxGap = 200;
+    let gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+    //same randomized with gap
+    //create a new component and push to the obstacles array
+    myObstacles.push(new Component(10, height, "green", x, 0));
+    //position is x = canvas width and 0 is y at position 0
+    myObstacles.push(
+      new Component(10, x - height - gap, "green", x, height + gap)
+    );
+  }
 }
 
 //create object called player and initialize its class
